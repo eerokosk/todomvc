cb93dbce5dfcee6fd3406951896d861673a065db
Ari Laakso <ari.laakso@fnx.fi>
2016-08-15 19:04:43 +0300

    toggle all completed

--- 3. react-redux-axios-nodejs-mongodb/frontend/src/components/list.js
+++ 3. react-redux-axios-nodejs-mongodb/frontend/src/components/list.js
@@ -11,11 +11,31 @@ export default React.createClass({
     });
   },
 
+  handleToggleAll: function(e) {
+    // has all todos been completed or not
+    var allCompleted = this.props.todos.filter((todo) =>
+      todo.completed === true ).length === this.props.todos.length
+        ? true : false;
+
+    // toggle all completed states or set all as completed
+    this.props.todos.map((todo, key) => {
+      todo.completed = allCompleted ? !todo.completed : true;
+      this.props.todoActions.updateTodo(todo);
+    });
+  },
+
   render: function() {
     var self = this;
     return (
       <section className="main">
-        <input className="toggle-all" id="toggle-all" type="checkbox" />
+        <input
+          className="toggle-all"
+          id="toggle-all"
+          type="checkbox"
+          checked={this.props.todos.filter((todo) =>
+            todo.completed === true ).length === this.props.todos.length}
+          onChange={this.handleToggleAll}
+          />
         <label htmlFor="toggle-all">Mark all as complete</label>
         <ul className="todo-list">
           {this.props.todos.map((todo, key) => {
